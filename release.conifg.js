eb5ad4b64fd90268bb918db9aa498f97fb77e8bc
Fausto Sgobba <fausto.sgobba@leica-geosystems.com>
2022-10-12 12:17:24 +0200

    fix: formatting

--- release.conifg.js
+++ release.conifg.js
@@ -1,9 +1,9 @@
 module.exports = {
-  branches: 'master',
-  repositoryUrl: 'https://github.com/fausto-sgobba/github-actions-course-react',
+  branches: "master",
+  repositoryUrl: "https://github.com/fausto-sgobba/github-actions-course-react",
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
   ],
 };
