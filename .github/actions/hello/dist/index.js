736642aefd89bb2298205a624822863cbb1e1a47
Fausto Sgobba <fausto.sgobba@leica-geosystems.com>
2022-10-14 10:23:47 +0200

    ci: test core package features

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -18197,14 +18197,23 @@ module.exports = /******/ (function (modules, runtime) {
       const github = __webpack_require__(484);
 
       try {
+        core.debug("Hello action execution started");
+        core.warn("Example warning debug log");
+        core.error("Example error debug log");
+
         // throw new Error("intentional error");
         const name = core.getInput("who-to-greet");
+        core.setSecret(name); // for testing purposes
         console.log(`hello ${name}`);
 
         const time = new Date();
         core.setOutput("time", time.toTimeString());
 
+        core.startGroup("Logging github object");
         console.log(JSON.stringify(github, null, "\t"));
+        core.endGroup();
+
+        core.exportVariable("HELLO", "hello");
       } catch (error) {
         core.setFailed(error.message);
       }
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,14 +2,23 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
+  core.debug("Hello action execution started");
+  core.warn("Example warning debug log");
+  core.error("Example error debug log");
+
   // throw new Error("intentional error");
   const name = core.getInput("who-to-greet");
+  core.setSecret(name); // for testing purposes
   console.log(`hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("Logging github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
